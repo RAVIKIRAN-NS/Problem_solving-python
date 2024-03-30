471e3f4ccac639706d63eee604e53e42b26be7e4
RAVIKIRAN NS <140703924+RAVIKIRAN-NS@users.noreply.github.com>
2024-03-30 23:25:36 +0530

    Update python.py

--- Revision/python.py
+++ Revision/python.py
@@ -377,3 +377,19 @@ def main():
 main()
 '''
 
+## 2nd Approach
+
+# num = 9474
+# count = 0 
+# num_count = []
+# add = 0
+#
+# for i in str(num):
+#     count = pow(int(i),len(str(num)))
+#     # count = int(i) ** len(str(num))
+#     num_count.append(count)
+#     add += count
+#
+# print(num_count)
+# print(add)
+
